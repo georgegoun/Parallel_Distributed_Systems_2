575375115caf191ae40ab9ef47ef05631caaa14b
georgegoun <georgiosgounaris@gmail.com>
2021-12-24 18:01:45 +0200

    version_2.5

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@
 
 int main(int argc, char* argv[])
 {
-    // int data[] = { 20, 51, 12, 73, 94, 15, 26, 67, 88, 39, 90, 11, 52, 23, 64, 35, 76, 7, 18, 99, 40, 81, 22, 13, 44, 95, 6, 27, 38, 79, 0, 41 };
-    // int data_length= sizeof(data) / sizeof(data[0]);
+    // double data[] = { 20, 51, 12, 73, 94, 15, 26, 67, 88, 39, 90, 11, 52, 23, 64, 35, 76, 7, 18, 99, 40, 81, 22, 13, 44, 95, 6, 27, 38, 79, 0, 41 };
+    //   int data_length= sizeof(data) / sizeof(data[0]);
     int ierr, num_procs, my_id;
 
     // MPI
@@ -31,13 +31,13 @@ int main(int argc, char* argv[])
 
     dataImportPivot(my_id, num_procs, &proc_data, &data_length);
     distributeByMedian(my_id, num_procs, data_length, proc_data, 0, num_procs - 1);
-    selfValidation(my_id, num_procs, data_length / num_procs, proc_data);
+    selfValidation(my_id, num_procs, data_length, proc_data);
 
-    printf("_ID: %d Data: ", my_id);
-    for (int i = 0; i < (data_length); i++) {
-        printf("%.2lf ", proc_data[i]);
-    }
-    printf("\n");
+    // printf("_ID: %d Data: ", my_id);
+    // for (int i = 0; i < (data_length); i++) {
+    //     printf("%.2lf ", proc_data[i]);
+    // }
+    // printf("\n");
 
     ierr
         = MPI_Finalize();
